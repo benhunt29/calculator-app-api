28f73e23209004d5df2f1f32cac30bfe9034c47f
benhunt29 <benhunt29@gmail.com>
2018-10-02 11:37:26 -0500

    add createdAt to response

--- services/calculationsService.js
+++ services/calculationsService.js
@@ -2,7 +2,10 @@ const Calculation = require('../models/Calculation')
 
 const getCalculations = async ({find = { }, limit = 10, sort = {}} = {}) => {
   const calculations = await Calculation.find(find).limit(limit).sort(sort)
-  return calculations.map((item) => item.calculation)
+  return calculations.map((item) => {
+    const { calculation, createdAt } = item
+    return { calculation, createdAt }
+  })
 }
 
 module.exports = {
